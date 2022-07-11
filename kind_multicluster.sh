e93b56993105d43badfdab05e3d6922100f146ef
Mridul Gain <108064351+mridul-avesha@users.noreply.github.com>
2022-07-11 21:55:53 +0530

    create KIND clusters
    create multiple test clusters for running multi-cluster kubeslice e2e tests

---
+++ kind_multicluster.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ $(kind get clusters | grep controller) ];then
+  echo \
+'cluster already exists, skipping creation..
+Run the following to generate kubeconfig for the same
+
+  kind get kubeconfig --name controller > assets/kubeconfig/kind.yaml
+
+Then run `./run.sh` to start e2e tests
+'
+  exit 0
+fi
+
+clusters=(controller worker-1 worker-2)
+
+for cluster_name in ${clusters[@]}
+do
+    kind create cluster --name $cluster_name --config assets/kind/cluster.yaml --image kindest/node:v1.22.7
+done
+
+# Provide correct IP in kind profile, since worker operator cannot detect internal IP as nodeIp
+# sed -i "s/NodeIP:.*/NodeIP: $ip/g" profile/mg.yaml
+
+cp ~/.kube/config assets/kubeconfig/config
+
+for i in ${!clusters[@]}
+do
+    ip=$(docker inspect ${clusters[$i]}-control-plane | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+    yq -i ".clusters[$i].cluster.server=\"https://$ip:6443\"" assets/kubeconfig/config 
+done
