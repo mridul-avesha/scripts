59027da1aab781f789670830a807f6d0ce9c6fa6
Mridul Gain <mridul@aveshasystems.com>
2022-11-21 11:23:30 +0530

    bulk rename helper
    Script to help in bulk renaming while migrating from cypress 7.7.x to 11.0.x

---
+++ cypress_version_migration_helper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to help in bulk renaming while migrating from cypress 7.7.x to 11.0.x
+# Usage: 
+# 1) copy this script to a directory who (or whose child dir) have cypress spec files. (e.g integration/ dir)
+# 2) execute from terminal by: ./cypress_version_migration_helper.sh
+files=$(find . -name "*.spec.js")
+for file in $files; do 
+    mv -- "$file" "${file%.spec.js}.cy.js"
+done
